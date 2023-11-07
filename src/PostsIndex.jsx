e22198044298455540dac51f2d97454f69fdb8b7
adinareiter <144619417+adinareiter@users.noreply.github.com>
2023-11-07 18:46:29 -0500

    Changes posts in postsIndex to be buttons and adds css to buttons

--- src/PostsIndex.jsx
+++ src/PostsIndex.jsx
@@ -6,10 +6,12 @@ export function PostsIndex(props) {
       <div className="cards">
         {props.posts.map((post) => (
           <div key={post.id} className="card">
-            <h2>{post.title}</h2>
-            <img src={post.image} alt="post pics" />
-            <p>Body: {post.body} </p>
-            <button onClick={props.onPostShow}>Open Post</button>
+            <button onClick={props.onPostShow}>
+              <h2>{post.title}</h2>
+              <img src={post.image} alt="post pics" />
+              <p>Body: {post.body} </p>
+              {/* <button onClick={props.onPostShow}>Open Post</button> */}
+            </button>
           </div>
         ))}
       </div>
